dd16b4cf93cb048963297b03c6e41edc8ab87f7b
W.M.N.M DISANAYAKE <2023cs035@stu.ucsc.cmb.ac.lk>
2026-09-11 09:14:56 +0530

    Time: 8 ms (89.37%), Space: 58.2 MB (38.36%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -4,14 +4,21 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-    s=s.toLowerCase();
-    t=t.toLowerCase();
-
-    s_final= s.split('').sort().join('');
-    t_final= t.split('').sort().join('');
-
-    if(s_final===t_final){
-        return true;
+    if (s.length !== t.length) return false;
+    
+    const count = {};
+    
+    for (let char of s) {
+        count[char] = (count[char] || 0) + 1;
     }
-    return false;
+    
+    for (let char of t) {
+        if (!count[char]) {
+            return false;
+        }
+        
+        count[char]--;
+    }
+    
+    return true;
 };
