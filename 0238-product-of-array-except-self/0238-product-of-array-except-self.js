f0ffbc64a54de079ad6b82795b9f1ba47ccbeedb
W.M.N.M DISANAYAKE <2023cs035@stu.ucsc.cmb.ac.lk>
2026-09-23 10:32:18 +0530

    Time: 5 ms (75.98%), Space: 68.8 MB (83.3%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.js
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -2,20 +2,20 @@
  * @param {number[]} nums
  * @return {number[]}
  */
+
 function productExceptSelf(nums) {
     const length = nums.length;
     const answer = new Array(length);
 
     answer[0] = 1;
-    for (let i = 1; i < length; i++) {
-        answer[i] = nums[i - 1] * answer[i - 1];
-    }
+    for(let i = 1; i< length; i++){
+        answer[i] = nums[i-1]*answer[i-1];
 
-    let rightProduct = 1;
-    for (let i = length - 1; i >= 0; i--) {
-        answer[i] = answer[i] * rightProduct;
-        rightProduct *= nums[i];
     }
-
+    let right =1
+    for(let i= length-1; i >=0; i--){
+        answer[i] = answer[i] * right;
+        right= right *nums[i];
+    }
     return answer;
 }
